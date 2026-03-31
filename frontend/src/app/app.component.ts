df835fb3da0f1d1dbdee52c25bf385e531c0b0f0
Luka Ratković <luka.ratkovic09@gmail.com>
2026-03-31 18:24:01 +0200

    Architecture switch and connecting layers
    Switched to standalone component architecture in angular and connected frontend with backend.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {RouterOutlet} from "@angular/router";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'budget-me-client';
+  private http = inject(HttpClient);
+
+  ngOnInit(): void {
+    this.http.get('/api/weatherforecast').subscribe(console.log)
+  }
+
 }
