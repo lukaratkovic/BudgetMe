dcdfec166e5364f6c6ff3a2bc883fdbd31a2ecf6
Luka Ratković <luka.ratkovic09@gmail.com>
2026-04-02 16:37:26 +0200

    Added navbar

---
+++ frontend/src/app/core/layout/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {MenubarModule} from "primeng/menubar";
+import {MenuItem} from "primeng/api";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, MenubarModule],
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.sass']
+})
+export class NavbarComponent implements OnInit {
+  items!: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'My Transactions',
+        icon: 'pi pi-credit-card',
+        routerLink: '/transactions'
+      }
+    ]
+  }
+}
