f114ef46644bfa75a55a599902c687204adba59a
Luka Ratković <luka.ratkovic09@gmail.com>
2026-04-06 22:59:44 +0200

    onUpload and onError events for transaction import

--- frontend/src/app/features/transactions/components/transaction-import-input/transaction-import-input.component.ts
+++ frontend/src/app/features/transactions/components/transaction-import-input/transaction-import-input.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {FileUpload, FileUploadModule} from "primeng/fileupload";
+import {DynamicDialogRef} from "primeng/dynamicdialog";
+import {NotificationService} from "../../../../core/services/notification.service";
 
 @Component({
   selector: 'app-transaction-import-input',
@@ -10,5 +12,15 @@ import {FileUpload, FileUploadModule} from "primeng/fileupload";
   styleUrls: ['./transaction-import-input.component.sass']
 })
 export class TransactionImportInputComponent {
+  private dialogRef = inject(DynamicDialogRef);
+  private notificationService = inject(NotificationService);
 
+  onUpload(event: any): void {
+    this.dialogRef.close(true);
+  }
+
+  onError(event: any): void {
+    let message = event.error.error.join('\n');
+    this.notificationService.displayError(message);
+  }
 }
